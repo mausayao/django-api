0ff19e1e65f98e3b53d405a7a5f804338580caf1
mauricio.sayao <mauricio.sayao@itbam.org.br>
2020-09-04 14:36:50 -0400

    Adicionado metodos get

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+from cursos.models import Curso, Avaliacao
+from cursos.serializers import CursoSerializer, AvaliacaoSerializer
+
+
+class CursoAPIView(APIView):
+    """
+    API Curso View
+    """
+
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+
+        return Response(serializer.data)
+
+
+class AvaliacaoAPIView(APIView):
+    """
+    API de Avaliação
+    """
+    
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
