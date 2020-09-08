12361d0851c44ce8c1058472b5256c7d9bee9ff7
mauricio.sayao <mauricio.sayao@itbam.org.br>
2020-09-08 09:10:05 -0400

    Adicionado metodo post

--- cursos/views.py
+++ cursos/views.py
@@ -1,6 +1,8 @@
+from django.db.migrations import serializer
+from django.template.context_processors import request
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
 
 from cursos.models import Curso, Avaliacao
 from cursos.serializers import CursoSerializer, AvaliacaoSerializer
@@ -17,6 +19,15 @@ class CursoAPIView(APIView):
 
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"msg": "Criou com sucesso"}, status=status.HTTP_201_CREATED)
+
+        
+
+
 
 class AvaliacaoAPIView(APIView):
     """
@@ -27,3 +38,9 @@ class AvaliacaoAPIView(APIView):
         avaliacoes = Avaliacao.objects.all()
         serializer = AvaliacaoSerializer(avaliacoes, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
