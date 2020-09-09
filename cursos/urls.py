b935bee9bda90c48151b50284411661759ac81e2
mauricio.sayao <mauricio.sayao@itbam.org.br>
2020-09-09 15:57:37 -0400

    Adicionado lista de avaliacoes por curso

--- cursos/urls.py
+++ cursos/urls.py
@@ -5,7 +5,10 @@ from cursos.views import CursoAPIView, CursosAPIView, AvaliacaoAPIView, Avaliaco
 
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(), name='cursos'),
+    path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
+    path('cursos/<int:curso_pk>/avaliacoes/', AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='curso_avaliacao'),
+    
     path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
-    path('cursos/<int:pk>', CursoAPIView.as_view(), name='curso'),
-    path('avaliacoes/<int:pk>', AvaliacaoAPIView.as_view(), name='avaliacao'),
+    path('avaliacoes/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='avaliacao'),
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from cursos.models import Curso, Avaliacao
 from cursos.serializers import CursoSerializer, AvaliacaoSerializer
@@ -18,8 +19,20 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        return self.queryset.all()
+
 
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(),
+             curso_id=self.kwargs.get('curso_pk'), pk=self.kwargs.get('avaliacao_pk'))
+        
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
 
