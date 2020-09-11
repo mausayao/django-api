e389711ecbb6e3c8aaa384f93c995026de59ab5d
mausayao <mauricio.sayao@gmail.com>
2020-09-11 10:17:16 -0400

    Adicionado autenticacao via token

--- coreproject/settings.py
+++ coreproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'django_filters',
     'rest_framework',
+    'rest_framework.authtoken',
 
     'cursos',
 ]
@@ -131,7 +132,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #DRF
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSE': (
-        'rest_framework.permissions.SessionAuthentication',
+        # 'rest_framework.permissions.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
