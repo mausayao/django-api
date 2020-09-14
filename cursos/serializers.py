02215492b100a6115ecc7e02ffeec80c9fbfdf79
mausayao <mauricio.sayao@gmail.com>
2020-09-14 13:51:50 -0400

    Adicionado validacao de campo avaliacao

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -22,6 +22,11 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
             'criacao',
             'ativo',
         )
+    
+    def validate_avaliacao(self, valor):
+        if valor in range(1, 6):
+            return valor
+        raise serializers.ValidationError('A avaliação precisa ser um número inteiro entre 1 e 5')
 
 
 class CursoSerializer(serializers.ModelSerializer):
