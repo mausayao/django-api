90ce799b3cd8633cea95ef9c50ecf775b14023e1
mausayao <mauricio.sayao@gmail.com>
2020-09-14 14:28:17 -0400

    Adicionado media de curso

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.db.models import Avg
 
 from cursos.models import Curso, Avaliacao
 
@@ -38,6 +39,8 @@ class CursoSerializer(serializers.ModelSerializer):
 
     # Primary key related field
     avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    media_avaliacoes = serializers.SerializerMethodField()
     class Meta:
         model = Curso
         fields = (
@@ -46,5 +49,14 @@ class CursoSerializer(serializers.ModelSerializer):
             'url',
             'criacao',
             'ativo',
-            'avaliacoes'
+            'avaliacoes',
+            'media_avaliacoes',
         )
+
+    
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+        if media is None:
+            return 0
+
+        return round(media * 2) / 2
